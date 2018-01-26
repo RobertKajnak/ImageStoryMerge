0cb6cd4104fea6ed17286c28cec3cf64ceca94ea
robertkajnak <hezirisz@gmail.com>
2018-01-26 12:28:42 +0100

    added complete zoom functionality

--- PhotoStoryMerge/PreviewForm.Designer.cs
+++ PhotoStoryMerge/PreviewForm.Designer.cs
@@ -30,6 +30,15 @@
         {
             this.menuStrip = new System.Windows.Forms.MenuStrip();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scaleDownBy25ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scaleToCurrentZoomLevellevelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomOutCtrlScrollUpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomInCtrlScrollUpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetZoomToActualSizeCtrlMiddleClickToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
@@ -42,6 +51,8 @@
             // 
             this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.saveToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
             this.closeToolStripMenuItem});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
@@ -55,6 +66,73 @@
             this.saveToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sToolStripMenuItem,
+            this.scaleDownBy25ToolStripMenuItem,
+            this.scaleToCurrentZoomLevellevelToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // sToolStripMenuItem
+            // 
+            this.sToolStripMenuItem.Name = "sToolStripMenuItem";
+            this.sToolStripMenuItem.Size = new System.Drawing.Size(267, 22);
+            this.sToolStripMenuItem.Text = "Scale Up by 25%";
+            // 
+            // scaleDownBy25ToolStripMenuItem
+            // 
+            this.scaleDownBy25ToolStripMenuItem.Name = "scaleDownBy25ToolStripMenuItem";
+            this.scaleDownBy25ToolStripMenuItem.Size = new System.Drawing.Size(267, 22);
+            this.scaleDownBy25ToolStripMenuItem.Text = "Scale Down by 25%";
+            // 
+            // scaleToCurrentZoomLevellevelToolStripMenuItem
+            // 
+            this.scaleToCurrentZoomLevellevelToolStripMenuItem.Name = "scaleToCurrentZoomLevellevelToolStripMenuItem";
+            this.scaleToCurrentZoomLevellevelToolStripMenuItem.Size = new System.Drawing.Size(267, 22);
+            this.scaleToCurrentZoomLevellevelToolStripMenuItem.Text = "Scale to current zoom level (<level>)";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoomOutCtrlScrollUpToolStripMenuItem,
+            this.zoomInCtrlScrollUpToolStripMenuItem,
+            this.zoomToToolStripMenuItem,
+            this.resetZoomToActualSizeCtrlMiddleClickToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // zoomOutCtrlScrollUpToolStripMenuItem
+            // 
+            this.zoomOutCtrlScrollUpToolStripMenuItem.Name = "zoomOutCtrlScrollUpToolStripMenuItem";
+            this.zoomOutCtrlScrollUpToolStripMenuItem.Size = new System.Drawing.Size(290, 22);
+            this.zoomOutCtrlScrollUpToolStripMenuItem.Text = "Zoom Out (Ctrl+Scroll Down)";
+            this.zoomOutCtrlScrollUpToolStripMenuItem.Click += new System.EventHandler(this.zoomOutCtrlScrollUpToolStripMenuItem_Click);
+            // 
+            // zoomInCtrlScrollUpToolStripMenuItem
+            // 
+            this.zoomInCtrlScrollUpToolStripMenuItem.Name = "zoomInCtrlScrollUpToolStripMenuItem";
+            this.zoomInCtrlScrollUpToolStripMenuItem.Size = new System.Drawing.Size(290, 22);
+            this.zoomInCtrlScrollUpToolStripMenuItem.Text = "Zoom In (Ctrl + Scroll Up)";
+            this.zoomInCtrlScrollUpToolStripMenuItem.Click += new System.EventHandler(this.zoomInCtrlScrollUpToolStripMenuItem_Click);
+            // 
+            // zoomToToolStripMenuItem
+            // 
+            this.zoomToToolStripMenuItem.Name = "zoomToToolStripMenuItem";
+            this.zoomToToolStripMenuItem.Size = new System.Drawing.Size(290, 22);
+            this.zoomToToolStripMenuItem.Text = "Zoom To ... (Z)";
+            this.zoomToToolStripMenuItem.Click += new System.EventHandler(this.zoomToToolStripMenuItem_Click);
+            // 
+            // resetZoomToActualSizeCtrlMiddleClickToolStripMenuItem
+            // 
+            this.resetZoomToActualSizeCtrlMiddleClickToolStripMenuItem.Name = "resetZoomToActualSizeCtrlMiddleClickToolStripMenuItem";
+            this.resetZoomToActualSizeCtrlMiddleClickToolStripMenuItem.Size = new System.Drawing.Size(290, 22);
+            this.resetZoomToActualSizeCtrlMiddleClickToolStripMenuItem.Text = "Reset Zoom To Actual Size (Middle Click)";
+            this.resetZoomToActualSizeCtrlMiddleClickToolStripMenuItem.Click += new System.EventHandler(this.resetZoomToActualSizeCtrlMiddleClickToolStripMenuItem_Click);
             // 
             // closeToolStripMenuItem
             // 
@@ -72,6 +150,8 @@
             this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox.TabIndex = 1;
             this.pictureBox.TabStop = false;
+            this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.flowLayoutPanel_MouseClick);
+            this.pictureBox.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.PreviewForm_MouseWheel);
             this.pictureBox.Resize += new System.EventHandler(this.pictureBox1_Resize);
             // 
             // flowLayoutPanel
@@ -82,6 +162,9 @@
             this.flowLayoutPanel.Name = "flowLayoutPanel";
             this.flowLayoutPanel.Size = new System.Drawing.Size(409, 277);
             this.flowLayoutPanel.TabIndex = 2;
+            this.flowLayoutPanel.WrapContents = false;
+            this.flowLayoutPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.flowLayoutPanel_MouseClick);
+            this.flowLayoutPanel.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.PreviewForm_MouseWheel);
             // 
             // PreviewForm
             // 
@@ -113,5 +196,14 @@
         private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomOutCtrlScrollUpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomInCtrlScrollUpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scaleDownBy25ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scaleToCurrentZoomLevellevelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomToToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetZoomToActualSizeCtrlMiddleClickToolStripMenuItem;
     }
 }
--- PhotoStoryMerge/PreviewForm.cs
+++ PhotoStoryMerge/PreviewForm.cs
@@ -14,11 +14,25 @@ namespace PhotoStoryMerge
     public partial class PreviewForm : Form
     {
         bool isControlPressed = false;
+        Size originalSize;
+
+        // maximum percentage available of screen resolution on resize
+        double p = 0.85; 
 
         public PreviewForm(Image image)
         {
             InitializeComponent();
             pictureBox.Image = image;
+
+            ///this is relevant for the zooming, but not resizing section
+            ///<see cref="zoomInCtrlScrollUpToolStripMenuItem_Click(object, EventArgs)"/>
+            ///this way, the image is loaded immediately rescaled. Using the zoom by default and then changing
+            ///the image would result in it loading in a small factor than rescaling. Loading the image 
+            ///first, rescaling box, then displaying it would require modifying InitializeComponent(), which
+            ///is I would rather not, both for cleanliness and safety
+            originalSize = new Size(image.Size.Width, image.Size.Height);
+            pictureBox.Size = new Size(image.Size.Width, image.Size.Height);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
@@ -59,10 +73,13 @@ namespace PhotoStoryMerge
             int H = Screen.PrimaryScreen.Bounds.Height;
             int w = pictureBox.Size.Width;
             int h = pictureBox.Size.Height;
-            double p = 0.85; // maximum percentage available
             if (w < W * p)
             {
-                 changeWidth(w + 20);
+                int scrollBarCorrection = 40;
+                ///menustrip is not detected in scroll height for some reason
+                if (pictureBox.Height /*+ menuStrip.Height*/ > H)
+                    scrollBarCorrection -= 20;
+                changeWidth(w + scrollBarCorrection);
             }
             else
             {
@@ -71,7 +88,7 @@ namespace PhotoStoryMerge
 
             if (h < H * p)
             {
-                changeHeight( h + 20);
+                changeHeight( h -30);
             }
             else
             {
@@ -81,17 +98,37 @@ namespace PhotoStoryMerge
 
         private void changeWidth(int newWidth)
         {
-            this.Width = newWidth;
-            flowLayoutPanel.Width = newWidth-17;
+            if (newWidth > 250)
+            {
+                this.Width = newWidth;
+                int scrollBarCorrection = 0;
+                if (pictureBox.Height > this.Height - 42)
+                    scrollBarCorrection += 17;
+                
+                flowLayoutPanel.Width = newWidth - scrollBarCorrection;
+            }
         }
         private void changeHeight(int newHeight)
         {
-            this.Height = newHeight;
-            flowLayoutPanel.Height = newHeight - menuStrip.Height-42;
+            if (newHeight > 200)
+            {
+                this.Height = newHeight;
+                flowLayoutPanel.Height = newHeight - menuStrip.Height - 42;
+            }
         }
 
         private void PreviewForm_Resize(object sender, EventArgs e)
         {
+            /*int W = Screen.PrimaryScreen.Bounds.Width;
+            int w = pictureBox.Size.Width;
+            int H = Screen.PrimaryScreen.Bounds.Height;
+                        
+            int scrollBarCorrection = 40;
+            ///menustrip is not detected in scroll height for some reason
+            if (pictureBox.Height > H)
+                scrollBarCorrection -= 20;
+            changeWidth(w + scrollBarCorrection);*/
+        
             changeHeight(this.Height);
             changeWidth(this.Width);
         }
@@ -112,6 +149,9 @@ namespace PhotoStoryMerge
         {
             switch (e.KeyCode)
             {
+                case (Keys.Z):
+                    zoomToToolStripMenuItem_Click(sender,e);
+                    break;
                 case (Keys.S):
                 case (Keys.Enter):
                     if (isControlPressed)
@@ -132,6 +172,152 @@ namespace PhotoStoryMerge
                     break;
             }
         }
+        ///TODO fix annoying thing with srollbars when zooming
+        ///TODO add resize functionality
+        ///TODO fix bug with background.png
+        private void zoomOutCtrlScrollUpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            setZoomTo(.8, false);
+        }
+
+        /// <summary>
+        /// Changes the current zoom level/picturebox size
+        /// </summary>
+        /// <param name="percentage">in decimals (e.g. 100% == 1)</param>
+        /// <param name="ofOriginal">true == percantege*original size ; false== percentage of current size</param>
+        private void setZoomTo(double percentage, bool ofOriginal)
+        {
+            if (percentage == 0 || percentage > 20)
+                return;
+            int w, h;
+            if (ofOriginal)
+            {
+                w = originalSize.Width;
+                h = originalSize.Height;
+            }
+            else
+            {
+                w = pictureBox.Size.Width;
+                h = pictureBox.Size.Height;
+            }
+            int newW = Convert.ToInt32(w * percentage);
+            int newH = Convert.ToInt32(h * percentage);
+            int maxW = originalSize.Width * 8;
+            int maxH = originalSize.Height * 8;
+            int minH = 100;
+            int minW = 100;
+            if (percentage < 1)
+            {
+                if (newW > minW && newH > minH)
+                {
+                    ///.8 and 1.25 so that the dimensions keep the same and zoomLevel works properly
+                    pictureBox.Size = new Size(newW,newH );
+                }
+                else
+                {
+                    if (newW < newH)
+                    {
+                        pictureBox.Size = new Size(minW, minW*newH/newW);
+                    }
+                    else
+                    {
+                        pictureBox.Size = new Size(minH * newW / newH, minH) ;
+                    }
+                }
+            }
+            else if (percentage > 1)
+            {
+                ///this prevents some bug when increasing the size too much and edges are added to the flowlayout
+                if (newH < maxH && newW < maxW)
+                {
+                    pictureBox.Size = new Size(newW, newH);
+                }
+                else
+                {
+                    pictureBox.Size = new Size(maxW, maxH);
+                }
+            }
+            else
+            {
+                pictureBox.Size = new Size(w, h);
+            }
+        }
+
+        private void zoomInCtrlScrollUpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            setZoomTo(1.25, false);
+        }
+
+        private void PreviewForm_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (isControlPressed)
+            {
+                if (e.Delta > 0)
+                {
+                    setZoomTo(1.25, false);
+                }
+                ///just in case there was an other event like click or somethign unpredicted
+                else if (e.Delta < 0)
+                {
+                    setZoomTo(.8, false);
+                }
+            }
+        }
+
+        private void zoomToToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string zoom = Prompt.ShowDialog("Set zoom to (%):", "Custom Zoom");
+            setZoomTo(Double.Parse(zoom)/100, true);
+        }
+
+        private void resetZoomToActualSizeCtrlMiddleClickToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            setZoomTo(1, true);
+        }
+        private void scaleUpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Middle)
+            {
+                setZoomTo(1, true);
+            }
+        }
+
+
+        public static class Prompt
+        {
+            public static string ShowDialog(string text, string caption)
+            {
+                Form prompt = new Form()
+                {
+                    Width = 500,
+                    Height = 150,
+                    FormBorderStyle = FormBorderStyle.FixedDialog,
+                    Text = caption,
+                    StartPosition = FormStartPosition.CenterScreen
+                };
+                Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+                TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+                Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+                confirmation.Click += (sender, e) => { prompt.Close(); };
+                prompt.Controls.Add(textBox);
+                prompt.Controls.Add(confirmation);
+                prompt.Controls.Add(textLabel);
+                prompt.AcceptButton = confirmation;
+
+                return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+            }
+        }
     }
-    
+
+
 }
